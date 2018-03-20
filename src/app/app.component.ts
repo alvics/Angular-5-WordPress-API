2c83346ba2648f94f11998720059b6c88a1071af
alvics <alvics71@hotmail.com>
2018-03-21 09:56:25 +1000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  posts = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+     this.http.get('https:YOUR SITE/wp-json/wp/v2/posts').subscribe(data => {
+       for (const key in data) {
+         if (data.hasOwnProperty(key)) {
+           this.posts.push(data[key]);
+         }
+       }
+       console.log(this.posts);
+     });
+
+  }
 }
